be052b4c43ac5f401e3dc4bbcae4b6e5163f8152
mahendra kumar <mahendra.dakshana15@gmail.com>
2017-02-27 21:13:50 +0530

    Update assignment_2_DP_problem2.cpp

--- assignment_2_DP_problem2.cpp
+++ assignment_2_DP_problem2.cpp
@@ -21,7 +21,8 @@ class Bad{
             nnode->data=d;
             nnode->link=NULL;
             first=nnode;
-            last=first->link;
+            last=first;
+            
             
         }
         else{node *nnode=new node();
@@ -31,15 +32,18 @@ class Bad{
             last=last->link;
             
         }
+        cout<<"--inserted"<<last->data<<"."<<endl;
     }
     bool checkBad(string s1,string s2){
         node *p=first;
-        bool b=false;
+        bool b=true;
         while(p){
             if((p->data==s1)||(p->data==s2))
             {
-                b=true;
-                return b;
+                b=false;
+                cout<<"equal"<<endl;
+                break;
+                
             }
             else
             p=p->link;
@@ -51,7 +55,7 @@ class Bad{
 
 class AvoidRoads{
     private :
-    long long count=1;
+    
     Bad Badroad;
     int width,height;
     public:
@@ -62,55 +66,100 @@ class AvoidRoads{
         cout<<"Enter number of bad road block"<<endl;
         cin>>badNum;
         for(int i=0;i<badNum;i++){
+            string s;
             int x1,y1,x2,y2;
             cin>>x1>>y1>>x2>>y2;
-            string s=change_to_string( x1, y1,x2,y2);
-            
+            s=change_to_string(x1,y1,x2,y2);
             Badroad.insert(s); 
+            cout<<endl;
         }
     }
-    string change_to_string(int x1,int y1,int x2,int y2){
+   
+     string change_to_string(int ,int,int,int);
+     long long checkpoint();
+     
+
+};
+ string AvoidRoads::change_to_string(int x1,int y1,int x2,int y2){
     string Result;         
    ostringstream convert;  
    convert <<x1<<" "<<y1<<" "<<x2<<" "<<y2;     
     Result = convert.str();
      return Result;
    }
-     void checkpoint(int i,int j){
-    if((i<=width)&&(j<=height)){
+   
+   
+long long AvoidRoads:: checkpoint(){
+long long T[height+2][width+2];
+for(int i=0;i<height+2;i++)
+T[i][width+1]=0;
+for(int j=0;j<width+2;j++)
+T[height+1][j]=0;
+
+for(int i=height;i>=0;i--){
+for(int j=width;j>=0;j--){
+    //at point where we have to reach
+	if(i==height&&j==width)
+	T[width][height]=1;
+	//at other points
+	if((i==height) &&(j!=width)){
+	string B1=change_to_string(i,j,i,j+1);
+      string B2=change_to_string(i,j+1,i,j);
+	bool b=Badroad.checkBad(B1,B2);
+	if(b){
+	T[i][j]=T[i][j+1];
+	}
+	else
+	T[i][j]=0;
+	
+	}
+	//---------
+	if(j==width && (i!=height)){
+	string A1= change_to_string(i,j,i+1,j) ;
+    string A2= change_to_string(i+1,j,i,j) ;
+	bool a=Badroad.checkBad(A1,A2);
+	if(a){
+	T[i][j]=T[i+1][j];
+	}
+	else
+	T[i][j]=0;
+}
+	//----
+	
+if(i<height && j<width){
       string A1= change_to_string(i,j,i+1,j) ;
       string A2= change_to_string(i+1,j,i,j) ;
       string B1=change_to_string(i,j,i,j+1);
       string B2=change_to_string(i,j+1,i,j);
-      bool a=~Badroad.checkBad(A1,A2);
-      bool b=~Badroad.checkBad(B1,B2);
-      int fork=0;
-      int first_count,second_count;
-      if(a&&(i+1<=height)){
-          fork++;
-          checkpoint(i+1,j);
-      }
-      if(b&&(j+1<=width)){
-          if(fork==1)
-          count++;
-          checkpoint(i,j+1);
-      }
-     
-      
-    }
-    
+      bool a=Badroad.checkBad(A1,A2); // for T[i+1][j]
+      bool b=Badroad.checkBad(B1,B2);  // for T[i][j+1]
+	  //both roads are open
+	  if(a&&b){
+	  T[i][j]=T[i][j+1]+T[i+1][j];
+	  }
+	  //if one road is block
+	  if(a==true&&b==false){
+	  T[i][j]=T[i+1][j];
+	  }
+	  if(a==false&&b==true){
+	  T[i][j]=T[i][j+1];
+	  }
+	  if(a==false && b==false)
+	  T[i][j]=0;
+
 }
-long long ReturnCount(){
-    return count;
+	
+	
+	}
 }
 
-};
+return T[0][0];
+}
+
 
 int main(){
     AvoidRoads A;
-    A.checkpoint(0,0);
-    long long max_Road=A.ReturnCount();
-    
+    long max_Road=A.checkpoint();
     cout<<max_Road<<endl;
     return 0;
 }
