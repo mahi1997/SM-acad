997dfa677604720607c2b5c991fac73fbdb0519c
mahendra kumar <mahendra.dakshana15@gmail.com>
2017-02-27 23:41:30 +0530

    Update assignment_2_DP_problem3.cpp

--- assignment_2_DP_problem3.cpp
+++ assignment_2_DP_problem3.cpp
@@ -1,95 +1,76 @@
-#include <iostream>
-
+#include<iostream>
+#include<string> 
 using namespace std;
-struct node{
-        node *link;
-        int data;
-        
-    };
-class CircularList{
-    
-    public:
-    node *head,*last;
-    CircularList(){
-        head=NULL;
-        last=NULL;
-    }
-    void insert(int d){
-        if(head==NULL){
-            node *nnode=new node();
-            nnode->data=d;
-            head=nnode;
-            last=nnode;
-            head->link=last;
-        }
-        else{node *nnode=new node();
-            nnode->data=d;
-            last->link=nnode;
-            nnode->link=head;
-            last=nnode;
-            
-        }
-    }
-    void display(){
-        cout<<head->data<<" ";
-        node* p=head->link;
-        while(p!=head){
-            cout<<p->data<<" ";
-            p=p->link;
-        }
-        cout<<endl;
-    }
-    
-};
-
-
-int longestIS(CircularList CList,int n){
-    int overall_max=0;
-    node* start=CList.head;
-    for(int k=0;k<n;k++){
-    //----
-    int *C=new int[n];
-    for(int i=0;i<n;i++)
-    C[i]=1;
-    int max=0;
-    node *q=start->link;
-    for(int i=1;i<n;i++){
-        node *p=start;
-        for(int j=0;j<i;j++){
-            if(p->data<q->data){
-                if(C[j]+1>C[i])
-                C[i]=C[j]+1;
-            }
-             p=p->link;
-            max=(max>C[i])?max:C[i];
-        }
-        q=q->link;
-    }
-    //----
-    start=start->link;
-    overall_max=(overall_max>max)?overall_max:max;
-    }
-    return overall_max;
-    
-    
+ 
+int binarySearch(long long A[], int left, int right, long long key) 
+{
+  	while(left + 1 < right) 
+		{
+  	 	 	int mid = left + (right - left) / 2;
+			if(A[mid]<key) 
+  	   		 left = mid;
+  	  		else 
+  	    		right = mid;
+  	 	 }
+ 	 return right;
 }
-
-int main(){
-    int T;
-    cin>>T;
-    int list[T];
-    for(int k=0;k<T;k++){
-    int N;
-    cin>>N;
-    CircularList C;
-    for(int i=0;i<N;i++){
-        int a;
-        cin>>a;
-        C.insert(a);
-    }
-    list[k]=longestIS( C,N);
-    }
-    for(int k=0;k<T;k++)
-    cout<<list[k]<<endl;
-    return 0;
+ 
+int LongIncreasingSubsequence(long long A[], int length) 
+{
+	  long long *temp = new long long[length];
+	  for(int i = 0; i < length; i++)
+  	  temp[i] = 0;
+	  int max_len;
+	  temp[0] = A[0];  //put starting as number at choosen  seat
+  	  max_len = 1;      //min length is only 1
+  	  for(int i = 0; i < length; ++i) 
+		{     
+		// if A[i] is less than starting number of sequence then make it smallest 
+		//smallest=temp[0]
+		//highest in sequence=temp[max-len-1]
+	    	 if(A[i] < temp[0])
+      				temp[0] = A[i];
+	    	 else if (A[i] > temp[max_len-1])  //if A[i] is greater than highest number in sequence than add it as highest and increase subsequence length
+      			{	
+				temp[max_len] = A[i];
+      				max_len++;
+			}
+    		 else
+			{   //arrange
+      				long long key = A[i];
+     				temp[binarySearch(temp, -1, max_len - 1, key)] = A[i];
+   		 	}
+		}
+	 delete[] temp;
+  	 return max_len;
 }
+ 
+int main() 
+{
+  	
+	int T;
+	cin>>T;
+	int ans[T];
+	for(int i=0;i<T;i++){
+			int N;
+   		    cin>>N;
+    		long long Array[2*N]; // 2*n so that we can choose any person from 1 to ..N
+ 		for(int j = 0; j < N; j++)
+		{
+      			cin>>Array[j];
+      			Array[j+N] = Array[j];
+   		}
+   	 int max = 0;
+   	 for(int j = 0; j< N; j++) 
+	{
+    	 	int a = LongIncreasingSubsequence(Array+j, N);
+      	 	if(a > max) 
+         	max = a;
+        }
+		ans[i]=max;
+	}
+	for(int i=0;i<T;i++)
+	cout<<ans[i]<<endl;
+	return 0;
+}
+ 
